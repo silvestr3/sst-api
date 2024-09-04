1a187e702a7375cef624b12e8bf94d4cdc9d3827
silvestr3 <fellipe.silvestre@outlook.com>
2024-09-04 10:31:57 -0300

    feat: Configure the EnvService with environment variables validation

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EnvService } from './env/env.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const env = app.get(EnvService);
+  const port = env.get('PORT');
+
+  await app.listen(port);
 }
 bootstrap();
