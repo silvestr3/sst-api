15a3defc8cd45a321d423517db33abbcbd7948b8
silvestr3 <fellipe.silvestre@outlook.com>
2024-09-04 11:01:28 -0300

    feat: Configure JWT authentication

---
+++ src/infra/auth/jwt-strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { z } from 'zod';
+import { EnvService } from '../env/env.service';
+
+const tokenSchema = z.object({
+  sub: z.string().uuid(),
+  subscription: z.string().uuid(),
+});
+
+export type UserPayload = z.infer<typeof tokenSchema>;
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(env: EnvService) {
+    const publicKey = env.get('JWT_PUBLIC_KEY');
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: publicKey,
+      algorithms: ['RS256'],
+    });
+  }
+
+  async validate(payload: UserPayload) {
+    return tokenSchema.parse(payload);
+  }
+}
